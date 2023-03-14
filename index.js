3560f664595ce78a5aa281134594f63e242bf672
williamferreiradev <119333308+williamferreiradev@users.noreply.github.com>
2023-03-13 21:10:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ links.forEach(link => {
     list.classList.remove('active');
   });
 });
+
+$(window).on('load', function() {
+  var progressBar = $('.barr');
+  var percent = $('.percent');
+  var interval = setInterval(function() {
+    var width = progressBar.width() + 10;
+    if (width >= 100) {
+      clearInterval(interval);
+      $('#loading').fadeOut(500);
+    } else {
+      progressBar.width(width + '%');
+      percent.text(width + '%');
+    }
+  }, 400);
+});
